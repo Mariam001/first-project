bcfa4587734521a4783d9125cdd8223538e50220
Mariam001 <43447813+Mariam001@users.noreply.github.com>
2019-10-19 19:19:44 +0400

    update

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+const obj = {
+  count: 0,
+  title: "text",
+  page: 5
+};
+
 const Header = () => {
-  return <h1>something</h1>;
+  return <h1>Text</h1>;
 };
 
 export default Header;
